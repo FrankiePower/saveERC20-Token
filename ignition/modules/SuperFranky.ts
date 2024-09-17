c2c34a1ae56dc6e98c8d2b14bd83428ee9ae216b
frankie-powers <franklin.power21@gmaul.com>
2024-09-17 08:49:56 +0100

    fixing my token

--- ignition/modules/SuperFranky.ts
+++ ignition/modules/SuperFranky.ts
@@ -1,7 +1,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const SuperFrankyModule = buildModule("SuperFrankyModule", (m) => {
-  const erc20 = m.contract("Web3CXI");
+  const erc20 = m.contract("SuperFranky");
 
   return { erc20 };
 });
