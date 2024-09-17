9daa3079dd607c93515dcfa807c13571a4785be3
frankie-powers <franklin.power21@gmaul.com>
2024-09-17 09:24:17 +0100

    cleaning up

--- ignition/modules/SaveERC20.ts
+++ ignition/modules/SaveERC20.ts
@@ -1,16 +1,11 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const tokenAddress = "0x6c5889875bFfa537F8c9bFD6e38161dc33AdD63f";
+const tokenAddress = "0x4c9B6c64664314D18C2cD05bF3FE31534c705C99";
 
 const SaveERC20Module = buildModule("SaveERC20Module", (m) => {
-  const save = m.contract("SaveERC20", [tokenAddress]);
+  const saveContract = m.contract("SaveERC20", [tokenAddress]);
 
-  return { save };
+  return { saveContract };
 });
 
 export default SaveERC20Module;
-
-// Deployed Addresses
-
-// Web3CXIModule#Web3CXI - 0x6c5889875bFfa537F8c9bFD6e38161dc33AdD63f
-// SaveERC20Module#SaveERC20 - 0x5E539d20AE218aAdEa7E0A7C54d28b59D60c5D5f
